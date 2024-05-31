c98616b9f7c417c249a59a5dd177ce2f70f9b761
hallowshaw <rhitam.rit54@gmail.com>
2024-06-01 01:02:14 +0530

    Bug fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -161,7 +161,7 @@ script.onload = function () {
 
 window.onload = async function () {
   try {
-    const response = await fetch('http://localhost:3000/api/get-views');
+    const response = await fetch('https://personal-portfolio-jukd.onrender.com/api/get-views');
     const data = await response.json();
     document.getElementById('viewCount').textContent = data.views;
   } catch (error) {
